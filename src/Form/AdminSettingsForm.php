6bc1d310cf82438d9c46efb13182f2ad22d167ce
Shabana Navas <Desam9@hotmail.com>
2018-04-27 17:02:40 +0530

    Added the admin settings for and the menu routes and links.

---
+++ src/Form/AdminSettingsForm.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Drupal\silverpop\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\silverpop\Entity\SilverpopSettings;
+
+/**
+ * Manage silverpop settings for this site.
+ */
+class AdminSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'silverpop_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['silverpop.admin_settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('silverpop.admin_settings');
+    $renderer = \Drupal::service('renderer');
+
+    $form['silverpop_tracking'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Silverpop Tracking'),
+      '#collapsible' => TRUE,
+    ];
+
+    $tracking_image_example_url = drupal_get_path('module', 'silverpop') . '/images/silverpop-web-tracking-code.png';
+
+    $tracking_image = [
+      '#theme' => 'image',
+      '#attributes' => ['style' => 'border: 1px solid #666; width: 50%;'],
+      '#alt' => $this->t('Web Tracking Code example.'),
+      '#uri' => $tracking_image_example_url,
+    ];
+
+    $form['silverpop_tracking']['silverpop_help'] = [
+      '#markup' => '<p>You will need to grab two values from the Silverpop web tracking
+      code to add tracking to this website.</p><p>' . $renderer->render($tracking_image) . '</p>',
+    ];
+
+    $form['silverpop_tracking']['silverpop_tracked_domains'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Tracked Domains'),
+      '#default_value' => $config->get('silverpop_tracked_domains'),
+      '#description' => 'Enter a comma-separated list of domains for Silverpop to
+      track from <a target="_blank" href="https://pilot.silverpop.com/viewOrganization.do">https://pilot.silverpop.com/viewOrganization.do</a>.',
+      '#rows' => 2,
+    ];
+
+    $form['silverpop_tracking']['silverpop_script_src'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Silverpop Script Source URL'),
+      '#default_value' => $config->get('silverpop_script_src'),
+      '#description' => 'Copy the source URL from the web tracking code on
+      <a target="_blank" href="https://pilot.silverpop.com/viewOrganization.do">https://pilot.silverpop.com/viewOrganization.do</a>.',
+      '#rows' => 2,
+    ];
+
+    $form['silverpop_events'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Silverpop Event Tracking'),
+      '#collapsible' => TRUE,
+    ];
+
+    $form['silverpop_events']['table'] = [
+      '#markup' => $this->silverpopOverview(),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('silverpop.admin_settings')
+      ->set('silverpop_tracked_domains', $form_state->getValue('silverpop_tracked_domains'))
+      ->set('silverpop_script_src', $form_state->getValue('silverpop_script_src'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Silverpop overview table.
+   */
+  public function silverpopOverview() {
+    $output = '';
+    $renderer = \Drupal::service('renderer');
+
+    // Fetch the existing events from the db.
+    $silverpop_settings = SilverpopSettings::loadMultiple();
+
+    $add_new = [
+      '#theme' => 'item_list',
+      '#items' => [
+        Link::createFromRoute(
+          $this->t('Add New'), 'entity.silverpop_settings.add_form'
+        ),
+      ],
+    ];
+
+    if (!count($silverpop_settings)) {
+      $output = '<p>No Silverpop event tracking set up.</p>';
+      $output .= $renderer->render($add_new);
+    }
+    else {
+      // Themes the events into a nice table.
+      $table = $this->displaySilverpopSettings($silverpop_settings);
+
+      $output .= $renderer->render($table);
+      $output .= $renderer->render($add_new);
+    }
+
+    return $output;
+  }
+
+  /**
+   * Themes the silverpop events into a themed table.
+   *
+   * @param array $silverpop_settings
+   *   An array of silverpop_settings entities.
+   *
+   * @return array
+   *   The themed table array.
+   */
+  function displaySilverpopSettings(array $silverpop_settings) {
+    $header = [
+      ['data' => t('Event Type')],
+      ['data' => t('Event Name')],
+      ['data' => ''],
+    ];
+
+    foreach ($silverpop_settings as $silverpop_setting) {
+      /** @var \Drupal\silverpop\Entity\SilverpopSettings $silverpop_setting */
+      $rows[] = [
+        ['data' => $silverpop_setting->getEventName()],
+        ['data' => $silverpop_setting->getEventType()],
+        [
+          'data' =>
+            Link::createFromRoute(
+              t('Edit'), 'entity.silverpop_settings.edit_form', [
+                'silverpop_settings' => $silverpop_setting->id(),
+              ]
+            ),
+        ],
+      ];
+    }
+
+    $table = [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+
+    return $table;
+  }
+
+}
